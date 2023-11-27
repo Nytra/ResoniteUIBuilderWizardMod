f4a2c2aa3e7830ce86bed930147669172c3d2a88
Nytra <samueltscott@gmail.com>
2023-11-27 01:33:35 +0000

    Upload local files

--- UIBuilderWizardMod/UIBuilderWizardMod.cs
+++ UIBuilderWizardMod/UIBuilderWizardMod.cs
@@ -5,6 +5,10 @@ using FrooxEngine.UIX;
 using Elements.Core;
 using Elements.Assets;
 using System;
+using System.Reflection;
+using HarmonyLib;
+using static FrooxEngine.UIX.LayoutHelper;
+using System.Security.Cryptography;
 
 namespace UIBuilderWizardMod
 {
@@ -33,12 +37,18 @@ namespace UIBuilderWizardMod
 				return new UIBuilderWizard(x);
 			}
 
+			private static FieldInfo rootsField = AccessTools.Field(typeof(UIBuilder), "roots");
+			private static FieldInfo uiStylesField = AccessTools.Field(typeof(UIBuilder), "_uiStyles");
+
 			Slot WizardSlot;
 			Slot WizardContentSlot;
+			RectTransform WizardContentRect;
 			Slot WizardDataSlot;
 			UIBuilder WizardUI;
-
+			//UIStyle WizardStyle;
+			
 			UIBuilder currentBuilder;
+			//IWorldElement lastElement;
 
 			// ===== initial screen =====
 
@@ -51,16 +61,25 @@ namespace UIBuilderWizardMod
 			// texts
 			Text rootText;
 			Text currentText;
+			//Text styleText;
+
+			// extra
+			Button openRootSlotButton;
+			Button openCurrentSlotButton;
+
+			Button editStyleButton;
+			//ValueField<bool> openWorkerInspectors;
 
 			// layout
 			Button verticalLayoutButton;
 			Button horizontalLayoutButton;
 			Button scrollAreaButton;
 			Button spacerButton;
+			Button scrollingVerticalLayoutButton;
 
 			// graphics
 			Button imageButton;
-			ValueField<colorX> imageColor;
+			//ValueField<colorX> imageColor;
 			Button textButton;
 
 			// interaction
@@ -81,43 +100,48 @@ namespace UIBuilderWizardMod
 				WizardSlot = x;
 				WizardSlot.Tag = "Developer";
 				WizardSlot.PersistentSelf = false;
-				WizardSlot.LocalScale *= 0.0008f;
+				WizardSlot.LocalScale *= 0.0006f;
 
 				WizardDataSlot = WizardSlot.AddSlot("Data");
 
-				WizardUI = RadiantUI_Panel.SetupPanel(WizardSlot, WIZARD_TITLE.AsLocaleKey(), new float2(800f, 1200f));
+				WizardUI = RadiantUI_Panel.SetupPanel(WizardSlot, WIZARD_TITLE.AsLocaleKey(), new float2(800f, 1500f));
 				RadiantUI_Constants.SetupEditorStyle(WizardUI);
 
 				WizardUI.Canvas.MarkDeveloper();
 				WizardUI.Canvas.AcceptPhysicalTouch.Value = false;
 
-				WizardUI.Style.MinHeight = 24f;
+				//WizardUI.Style.MinHeight = 24f;
 				WizardUI.Style.PreferredHeight = 24f;
-				WizardUI.Style.PreferredWidth = 400f;
-				WizardUI.Style.MinWidth = 400f;
+				WizardUI.Style.PreferredWidth = 96f;
+				//WizardUI.Style.MinWidth = 400f;
 
 				WizardSlot.PositionInFrontOfUser(float3.Backward, distance: 1f);
 
 				WizardContentSlot = WizardUI.Root;
+				WizardContentRect = WizardUI.CurrentRect;
+
+				//WizardStyle = WizardUI.Style.Clone();
 
 				RegenerateWizardUI();
 			}
 
 			void RegenerateWizardUI()
 			{
-				//WizardDataSlot.DestroyChildren();
+				WizardDataSlot.DestroyChildren();
 				WizardContentSlot.DestroyChildren();
-				var rect = WizardContentSlot.GetComponent<RectTransform>();
-				WizardUI.ForceNext = rect;
-				WizardContentSlot.RemoveAllComponents((Component c) => c != rect);
+				//var rect = WizardContentSlot.GetComponent<RectTransform>();
+				WizardUI.ForceNext = WizardContentRect;
+				WizardContentSlot.RemoveAllComponents((Component c) => c != WizardContentRect);
 
 				if (!ValidateCurrentBuilder())
 				{
 					// build initial screen
 
-					panelName = WizardDataSlot.FindChildOrAdd("Panel Name").AttachComponent<ValueField<string>>();
+					//WizardUI.PushStyle();
+
+					panelName = WizardDataSlot.FindChildOrAdd("Panel Name").GetComponentOrAttach<ValueField<string>>();
 					panelName.Value.Value = "Test UIX Panel";
-					panelSize = WizardDataSlot.FindChildOrAdd("Panel Size").AttachComponent<ValueField<float2>>();
+					panelSize = WizardDataSlot.FindChildOrAdd("Panel Size").GetComponentOrAttach<ValueField<float2>>();
 					panelSize.Value.Value = new float2(800f, 800f);
 
 					VerticalLayout verticalLayout = WizardUI.VerticalLayout(4f, childAlignment: Alignment.TopCenter);
@@ -126,10 +150,26 @@ namespace UIBuilderWizardMod
 					SyncMemberEditorBuilder.Build(panelName.Value, "Panel Name", null, WizardUI);
 					SyncMemberEditorBuilder.Build(panelSize.Value, "Panel Size", null, WizardUI);
 
+					//GenerateStyleMemberEditors(WizardUI, WizardUI.Style);
+
 					WizardUI.Spacer(24f);
 
 					createPanelButton = WizardUI.Button("Create Panel");
-					createPanelButton.LocalPressed += CreatePanel;
+					createPanelButton.LocalPressed += (btn, data) => 
+					{
+						Slot root = WizardSlot.LocalUserSpace.AddSlot(panelName.Value.Value);
+						currentBuilder = CreatePanel(root, root.Name, panelSize.Value.Value);
+						currentBuilder.Root.OnPrepareDestroy += (slot) => 
+						{
+							WizardSlot.RunSynchronously(() => 
+							{ 
+								WizardAction(null, new ButtonEventData(), () => { }); 
+							});
+						};
+						CopyStyle(WizardUI, currentBuilder);
+						//WizardUI.PopStyle();
+						RegenerateWizardUI();
+					};
 				}
 				else
 				{
@@ -143,6 +183,21 @@ namespace UIBuilderWizardMod
 
 					rootText = WizardUI.Text("");
 					currentText = WizardUI.Text("");
+					//styleText = WizardUI.Text("");
+
+					openRootSlotButton = WizardUI.Button("Open Root Slot");
+					openRootSlotButton.LocalPressed += OpenRootSlot;
+
+					openCurrentSlotButton = WizardUI.Button("Open Current Slot");
+					openCurrentSlotButton.LocalPressed += OpenCurrentSlot;
+
+					WizardUI.Spacer(24f);
+
+					editStyleButton = WizardUI.Button("Edit Style");
+					editStyleButton.LocalPressed += EditStyle;
+
+					//openWorkerInspectors = WizardDataSlot.FindChildOrAdd("Open Worker Inspectors").GetComponentOrAttach<ValueField<bool>>();
+					//WizardUI.HorizontalElementWithLabel("Open Worker Inspectors:", 0.942f, () => WizardUI.BooleanMemberEditor(openWorkerInspectors.Value));
 
 					WizardUI.Spacer(24f);
 
@@ -150,6 +205,19 @@ namespace UIBuilderWizardMod
 
 					verticalLayoutButton = WizardUI.Button("Vertical Layout");
 					verticalLayoutButton.LocalPressed += AddVerticalLayout;
+
+					//scrollingVerticalLayoutButton = WizardUI.Button("Scrolling Vertical Layout");
+					//scrollingVerticalLayoutButton.LocalPressed += AddScrollingVerticalLayout;
+
+					//Button newVerticalLayoutButton = WizardUI.Button("New Vertical Layout");
+					//               newVerticalLayoutButton.LocalPressed += (btn, data) => 
+					//{ 
+					//	WizardAction(btn, data, () => 
+					//	{
+					//		MethodInfo method = AccessTools.Method(typeof(UIBuilder), "VerticalLayout", new Type[] { typeof(float), typeof(float), typeof(float), typeof(float), typeof(float), typeof(Alignment?) });
+					//		TestPanelWithArgs(method);
+					//                   });
+					//};
 
 					horizontalLayoutButton = WizardUI.Button("Horizontal Layout");
 					horizontalLayoutButton.LocalPressed += AddHorizontalLayout;
@@ -164,11 +232,11 @@ namespace UIBuilderWizardMod
 
 					WizardUI.Text("Graphics");
 
-					imageColor = WizardDataSlot.FindChildOrAdd("Image Color").AttachComponent<ValueField<colorX>>();
-					imageColor.Value.Value = colorX.White;
+					//imageColor = WizardDataSlot.FindChildOrAdd("Image Color").GetComponentOrAttach<ValueField<colorX>>();
+					//imageColor.Value.Value = colorX.White;
 
-					WizardUI.Text("Color:").HorizontalAlign.Value = TextHorizontalAlignment.Left;
-					SyncMemberEditorBuilder.Build(imageColor.Value, imageColor.Value.Name, null, WizardUI);
+					//WizardUI.Text("Color:").HorizontalAlign.Value = TextHorizontalAlignment.Left;
+					//SyncMemberEditorBuilder.Build(imageColor.Value, imageColor.Value.Name, null, WizardUI);
 
 					imageButton = WizardUI.Button("Image");
 					imageButton.LocalPressed += AddImage;
@@ -195,7 +263,7 @@ namespace UIBuilderWizardMod
 					refEditorButton = WizardUI.Button("RefEditor");
 					refEditorButton.LocalPressed += AddRefEditor;
 
-					memberEditorMember = WizardDataSlot.FindChildOrAdd("MemberEditor Member").AttachComponent<ReferenceField<ISyncMember>>();
+					memberEditorMember = WizardDataSlot.FindChildOrAdd("MemberEditor Member").GetComponentOrAttach<ReferenceField<ISyncMember>>();
 
 					WizardUI.Text("SyncMember:").HorizontalAlign.Value = TextHorizontalAlignment.Left;
 					WizardUI.Next("MemberEditor Member");
@@ -214,7 +282,7 @@ namespace UIBuilderWizardMod
 					nestOutButton = WizardUI.Button("Nest Out");
 					nestOutButton.LocalPressed += NestOut;
 
-					nestIntoSlot = WizardDataSlot.FindChildOrAdd("Nest Into Slot").AttachComponent<ReferenceField<Slot>>();
+					nestIntoSlot = WizardDataSlot.FindChildOrAdd("Nest Into Slot").GetComponentOrAttach<ReferenceField<Slot>>();
 
 					WizardUI.Text("Slot:").HorizontalAlign.Value = TextHorizontalAlignment.Left;
 					WizardUI.Next("Nest Into Slot");
@@ -229,8 +297,28 @@ namespace UIBuilderWizardMod
 
 			void UpdateTexts()
 			{
-				rootText.Content.Value = "Root: " + (currentBuilder.Root?.Name ?? "Null");
-				currentText.Content.Value = "Current: " + (currentBuilder.Current?.Name ?? "Null");
+				var roots = (Stack<Slot>)rootsField.GetValue(currentBuilder);
+				var uiStyles = (Stack<UIStyle>)uiStylesField.GetValue(currentBuilder);
+
+				rootText.Content.Value = $"[{roots.Count}] Root: ";
+				if (IsSlotValid(currentBuilder.Root))
+				{
+					rootText.Content.Value += currentBuilder.Root.Name;
+				}
+				else
+				{
+					rootText.Content.Value += "Null";
+				}
+				currentText.Content.Value = "Current: ";
+				if (IsSlotValid(currentBuilder.Current))
+				{
+					currentText.Content.Value += currentBuilder.Current.Name;
+				}
+				else
+				{
+					currentText.Content.Value += "Null";
+				}
+				//styleText.Content.Value = "Styles count: " + uiStyles.Count;
 			}
 
 			bool IsSlotValid(Slot s)
@@ -257,22 +345,150 @@ namespace UIBuilderWizardMod
 				return true;
 			}
 
-			void CreatePanel(IButton button, ButtonEventData eventData)
+			UIBuilder CreatePanel(Slot root, string name, float2 size)
 			{
-				Slot root = WizardSlot.LocalUserSpace.AddSlot(panelName.Value.Value);
-				currentBuilder = RadiantUI_Panel.SetupPanel(root, root.Name.AsLocaleKey(), panelSize.Value);
-				RadiantUI_Constants.SetupEditorStyle(currentBuilder);
+				UIBuilder builder = RadiantUI_Panel.SetupPanel(root, name.AsLocaleKey(), size);
+				RadiantUI_Constants.SetupEditorStyle(builder);
 				root.LocalScale *= 0.0005f;
 				root.PositionInFrontOfUser(float3.Backward, distance: 1f);
-				RegenerateWizardUI();
+				return builder;
+			}
+
+			void CopyStyle(UIBuilder b1, UIBuilder b2)
+			{
+				FieldInfo[] fields = typeof(UIStyle).GetFields();
+				int i = 0;
+				foreach (FieldInfo field in fields)
+				{
+					field.SetValue(b2.Style, field.GetValue(b1.Style));
+					i++;
+				}
+			}
+
+			void GenerateStyleMemberEditors(UIBuilder UI, UIStyle targetStyle)
+			{
+				FieldInfo[] fields = typeof(UIStyle).GetFields();
+				int i = 0;
+				foreach (FieldInfo field in fields)
+				{
+					Slot s = WizardDataSlot.FindChildOrAdd(i.ToString() + "_" + field.Name);
+					if (field.FieldType.IsValueType)
+					{
+						Type t = typeof(ValueField<>).MakeGenericType(field.FieldType);
+						Component c = s.GetComponent(t);
+						bool subscribe = false;
+						if (c == null)
+						{
+							c = s.AttachComponent(t);
+							subscribe = true;
+						}
+						ISyncMember member = c.GetSyncMember("Value");
+						((IField)member).BoxedValue = field.GetValue(targetStyle);
+						SyncMemberEditorBuilder.Build(member, field.Name, null, UI);
+						if (subscribe)
+						{
+							member.Changed += (value) => { field.SetValue(targetStyle, ((IField)value).BoxedValue); };
+						}
+					}
+					else
+					{
+						Type t = typeof(ReferenceField<>).MakeGenericType(field.FieldType);
+						Component c = s.GetComponent(t);
+						bool subscribe = false;
+						if (c == null)
+						{
+							c = s.AttachComponent(t);
+							subscribe = true;
+						}
+						ISyncMember member = c.GetSyncMember("Reference");
+						((ISyncRef)member).Target = (IWorldElement)field.GetValue(targetStyle);
+						SyncMemberEditorBuilder.Build(member, field.Name, null, UI);
+						if (subscribe)
+						{
+							member.Changed += (value) => { field.SetValue(targetStyle, ((ISyncRef)value).Target); };
+						}
+					}
+					i++;
+				}
+			}
+
+			void CreatePanelWithMethodParameters(MethodInfo method)
+			{
+				Slot root = WizardSlot.LocalUserSpace.AddSlot("Test Panel with Args");
+				UIBuilder UI = CreatePanel(root, root.Name, new float2(800, 800));
+
+				VerticalLayout verticalLayout = UI.VerticalLayout(4f, childAlignment: Alignment.TopCenter);
+				verticalLayout.ForceExpandHeight.Value = false;
+
+				ParameterInfo[] parameters = method.GetParameters();
+				int i = 0;
+				foreach (ParameterInfo param in parameters) 
+				{
+					Slot s = WizardDataSlot.FindChildOrAdd(i.ToString() + "_" + param.Name);
+					if (param.ParameterType.IsValueType)
+					{
+						Type t = typeof(ValueField<>).MakeGenericType(param.ParameterType);
+						Component c = s.AttachComponent(t);
+						SyncMemberEditorBuilder.Build(c.GetSyncMember("Value"), param.Name, null, UI);
+					}
+					else
+					{
+						Type t = typeof(ReferenceField<>).MakeGenericType(param.ParameterType);
+						Component c = s.AttachComponent(t);
+						SyncMemberEditorBuilder.Build(c.GetSyncMember("Reference"), param.Name, null, UI);
+					}
+					i++;
+				}
 			}
 
 			// ===== ACTIONS =====
 
+			void OpenRootSlot(IButton button, ButtonEventData eventData)
+			{
+				WizardAction(button, eventData, () =>
+				{
+					currentBuilder.Root.OpenInspectorForTarget();
+				});
+			}
+
+			void OpenCurrentSlot(IButton button, ButtonEventData eventData)
+			{
+				WizardAction(button, eventData, () =>
+				{
+					currentBuilder.Current.OpenInspectorForTarget();
+				});
+			}
+
+			void EditStyle(IButton button, ButtonEventData eventData)
+			{
+				WizardAction(button, eventData, () =>
+				{
+					Slot s = WizardSlot.LocalUserSpace.AddSlot("Style Edit Panel");
+					UIBuilder b = CreatePanel(s, s.Name, new float2(800, 1500));
+					b.Canvas.MarkDeveloper();
+					b.Canvas.AcceptPhysicalTouch.Value = false;
+					VerticalLayout verticalLayout = b.VerticalLayout(4f, childAlignment: Alignment.TopCenter);
+					verticalLayout.ForceExpandHeight.Value = false;
+					GenerateStyleMemberEditors(b, currentBuilder.Style);
+				});
+			}
+
 			void AddVerticalLayout(IButton button, ButtonEventData eventData)
 			{
 				WizardAction(button, eventData, () => 
-				{ 
+				{
+					VerticalLayout verticalLayout = currentBuilder.VerticalLayout();
+					//lastElement = verticalLayout;
+					verticalLayout.OpenInspectorForTarget(openWorkerOnly: true);
+				});
+			}
+
+			void AddScrollingVerticalLayout(IButton button, ButtonEventData eventData)
+			{
+				WizardAction(button, eventData, () =>
+				{
+					currentBuilder.ScrollArea();
+					currentBuilder.FitContent(SizeFit.Disabled, SizeFit.PreferredSize);
 					currentBuilder.VerticalLayout();
 				});
 			}
@@ -281,7 +497,9 @@ namespace UIBuilderWizardMod
 			{
 				WizardAction(button, eventData, () => 
 				{
-					currentBuilder.HorizontalLayout();
+					HorizontalLayout horizontalLayout = currentBuilder.HorizontalLayout();
+					//lastElement = horizontalLayout;
+					horizontalLayout.OpenInspectorForTarget(openWorkerOnly: true);
 				});
 			}
 
@@ -289,8 +507,12 @@ namespace UIBuilderWizardMod
 			{
 				WizardAction(button, eventData, () =>
 				{
-					currentBuilder.ScrollArea();
-					currentBuilder.FitContent();
+					ScrollRect scrollRect = currentBuilder.ScrollArea();
+					//lastElement = scrollRect;
+					scrollRect.OpenInspectorForTarget(openWorkerOnly: true);
+					ContentSizeFitter fitter = currentBuilder.FitContent();
+					//lastElement = fitter;
+					fitter.OpenInspectorForTarget(openWorkerOnly: true);
 				});
 			}
 
@@ -298,7 +520,8 @@ namespace UIBuilderWizardMod
 			{
 				WizardAction(button, eventData, () =>
 				{
-					currentBuilder.Spacer(24f);
+					RectTransform rect = currentBuilder.Spacer(24f);
+					//lastElement = rect;
 				});
 			}
 
@@ -306,7 +529,9 @@ namespace UIBuilderWizardMod
 			{
 				WizardAction(button, eventData, () =>
 				{
-					currentBuilder.Image(imageColor.Value.Value);
+					Image img = currentBuilder.Image();
+					img.OpenInspectorForTarget(openWorkerOnly: true);
+					//lastElement = img;
 				});
 			}
 
@@ -314,18 +539,29 @@ namespace UIBuilderWizardMod
 			{
 				WizardAction(button, eventData, () =>
 				{
-					currentBuilder.Text("Text");
+					Text t = currentBuilder.Text("Text");
+					t.OpenInspectorForTarget(openWorkerOnly: true);
+					//lastElement = t;
 				});
 			}
 
 			void AddButton(IButton button, ButtonEventData eventData)
 			{
-				WizardAction(button, eventData, () => { currentBuilder.Button("Button"); });
+				WizardAction(button, eventData, () => 
+				{ 
+					Button b = currentBuilder.Button("Button");
+					//lastElement = b;
+				});
 			}
 
 			void AddCheckbox(IButton button, ButtonEventData eventData)
 			{
-				WizardAction(button, eventData, () => { currentBuilder.Checkbox(); });
+				WizardAction(button, eventData, () => 
+				{
+					Checkbox checkbox = currentBuilder.Checkbox();
+					checkbox.OpenInspectorForTarget(openWorkerOnly: true);
+					//lastElement = checkbox;
+				});
 			}
 
 			void AddRefEditor(IButton button, ButtonEventData eventData)
@@ -333,7 +569,10 @@ namespace UIBuilderWizardMod
 				WizardAction(button, eventData, () => 
 				{
 					currentBuilder.Next("RefEditor");
-					currentBuilder.Current.AttachComponent<RefEditor>().Setup(null);
+					RefEditor refEditor = currentBuilder.Current.AttachComponent<RefEditor>();
+					refEditor.Setup(null);
+					//lastElement = refEditor;
+					refEditor.OpenInspectorForTarget(openWorkerOnly: true);
 				});
 			}
 
@@ -343,7 +582,6 @@ namespace UIBuilderWizardMod
 				{
 					if (memberEditorMember.Reference.Target != null)
 					{
-						//currentBuilder.Next("MemberEditor");
 						SyncMemberEditorBuilder.Build(memberEditorMember.Reference.Target, memberEditorMember.Reference.Target.Name, null, currentBuilder);
 					}
 				});
@@ -351,12 +589,18 @@ namespace UIBuilderWizardMod
 
 			void Nest(IButton button, ButtonEventData eventData)
 			{
-				WizardAction(button, eventData, () => { currentBuilder.Nest(); });
+				WizardAction(button, eventData, () => 
+				{ 
+					currentBuilder.Nest(); 
+				});
 			}
 
 			void NestOut(IButton button, ButtonEventData eventData)
 			{
-				WizardAction(button, eventData, () => { currentBuilder.NestOut(); });
+				WizardAction(button, eventData, () => 
+				{ 
+					currentBuilder.NestOut(); 
+				});
 			}
 
 			void NestInto(IButton button, ButtonEventData eventData)
@@ -374,8 +618,18 @@ namespace UIBuilderWizardMod
 			{
 				if (!ValidateCurrentBuilder())
 				{
-					RegenerateWizardUI();
-					return;
+					if (IsSlotValid(currentBuilder?.Canvas?.Slot))
+					{
+						while (!IsSlotValid(currentBuilder.Current) && !IsSlotValid(currentBuilder.Root))
+						{
+							currentBuilder.NestOut();
+						}
+					}
+					else
+					{
+						RegenerateWizardUI();
+						return;
+					}
 				}
 				action();
 				UpdateTexts();
